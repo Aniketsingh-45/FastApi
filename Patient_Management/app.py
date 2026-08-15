5e5262e89682477d38d6bd909341696e4e580650
Aniketsingh-45 <aniketsingh4500@gmail.com>
2026-08-15 19:47:16 +0530

    making patient management api

---
+++ Patient_Management/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import json
+
+app=FastAPI()
+
+def load_data():
+    with open('patient.json', 'r') as f:
+        data=json.load(f)
+
+    return data
+
+
+@app.get('/')
+def hello():
+    return{'message': 'Patient Management System Api'}
+
+@app.get('/about')
+def about():
+    return {'message': 'A fully functional patient management system api'}
+
+
+@app.get('/view')
+
+def view():
+    data=load_data()
+    return data
