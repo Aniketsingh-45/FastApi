25f5423a169157fa7d08d453d27dcaaa1998f1a1
Aniketsingh-45 <aniketsingh4500@gmail.com>
2026-08-15 21:04:59 +0530

    adding path and query params

--- Patient_Management/app.py
+++ Patient_Management/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi.openapi.utils import status_code_ranges
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app=FastAPI()
@@ -37,5 +38,25 @@ def view_patient(patient_id: str= Path(..., description="Enter Patient ID: ", ex
 
     raise HTTPException(status_code=404, detail='Patient not found')
    
+@app.get('/sort')
+
+def sort_patient(sort_by: str =Query (..., description='sort on the basis of weight, height, bmi or age'), order: str = Query('asc', description='ascending or descending')):
+    
+    valid_sort=['height','weight','bmi']
+
+    if sort_by not in valid_sort:
+        raise HTTPException(status_code=404 , detail='Invalid sorting parameter{valid_sort}') 
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=404, detail="Invalid orderparameter{order}")
+
+    data=load_data()
+
+    sort_order=True if order=="asc" else False
 
 
+    sorted_data=sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+ 
+    
